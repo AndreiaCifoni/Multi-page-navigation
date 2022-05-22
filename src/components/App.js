79d212de5f59a20bca2f8315d70efb59f7f875e1
Andreia Cifoni <andreiacifoni@gmail.com>
2022-05-22 14:59:34 -0400

    Routes added - new style of Routes with v6

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,21 @@
 import React from "react";
-import { Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Navbar";
+import Home from "./Home";
+import About from "./About";
+import Blog from "./Blog";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path={"/"} element={<Home />} />
+        <Route path={"/about"} element={<About />} />
+        <Route path={"/blog"} element={<Blog />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div>
+      <Link to={"/"}>Home</Link>
+      <Link to={"/about"}>About us</Link>
+      <Link to={"/blog"}>Blog</Link>
+    </div>
+  );
+};
+
+export default Navbar;
