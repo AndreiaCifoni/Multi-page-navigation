294bfd70d69241cd695d1973a2234ca3cc00be67
Andreia Cifoni <andreiacifoni@gmail.com>
2022-05-22 21:34:33 -0400

    css style added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,18 @@
 import React from "react";
+import "./style.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="secondary">
+      <h1>Welcome</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </p>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./style.css";
 
 const Navbar = () => {
   return (
-    <div>
-      <Link to={"/"}>Home</Link>
-      <Link to={"/about"}>About us</Link>
-      <Link to={"/blog"}>Blog</Link>
+    <div className="container">
+      <Link className="link" to={"/"}>
+        Home
+      </Link>
+      <Link className="link" to={"/about"}>
+        About us
+      </Link>
+      <Link className="link" to={"/blog"}>
+        Blog
+      </Link>
     </div>
   );
 };
